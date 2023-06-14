017337c4fdcd9e578f709ef054d89ea93c6a2988
fengliushijie <1099216627@qq.com>
2023-06-14 18:47:42 +0800

    🚀版本检查完成

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -3,12 +3,31 @@
 module.exports = core;
 
 const log = require("@jr-cli/log");
+const semver = require("semver");
+const colors = require("colors")
 const pkg = require("../package.json");
+const constant = require("./const");
+
 function core(args) {
-    checkPkgVersion();
+    try{
+        checkPkgVersion();
+        checkNodeVersion();
+    }catch (e) {
+        log.error(e.message)
+    }
 }
 
+// 检查脚手架版本
 function checkPkgVersion () {
     log.info("version",`当前版本号为${pkg.version}`)
-    console.log("发布测试代码")
 }
+
+// 检查node版本
+function checkNodeVersion () {
+    const curNodeVersion = process.version;
+    const lowestNodeVersion = constant.LOWEST_NODE_VERSION;
+    if(semver.lt(curNodeVersion,lowestNodeVersion)){
+        throw new Error(colors.red(`Node版本过低，请升级 ${lowestNodeVersion} 以上的版本`))
+    }
+}
+

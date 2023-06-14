90541a497c9d8af129686a617a525f4a536adb77
fengliushijie <1099216627@qq.com>
2023-06-14 19:24:37 +0800

    🚀参数和环境变量检查完成

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -4,12 +4,17 @@ module.exports = core;
 
 const log = require("@jr-cli/log");
 const semver = require("semver");
+const path = require("path");
 const colors = require("colors")
-const pkg = require("../package.json");
 const root = require("root-check")
 const userHome = require("user-home");
 const pathExists = require("path-exists").sync;
+const minimist = require("minimist");
+const env = require("dotenv");
+
 const constant = require("./const");
+const pkg = require("../package.json");
+
 
 function core(args) {
     try{
@@ -17,6 +22,8 @@ function core(args) {
         checkNodeVersion();
         rootDegradation();
         checkUserHome();
+        checkInputArgs(args);
+        checkEnv();
     }catch (e) {
         log.error(e.message)
     }
@@ -45,4 +52,40 @@ function checkUserHome () {
     if(!userHome || !pathExists(userHome)){
         throw new Error(colors.red("当前用户主目录不存在"))
     }
+}
+// 检查命令行参数
+function checkInputArgs (args) {
+    args = minimist(args);
+    checkArgs(args)
+}
+function checkArgs(args){
+    if(args.debug){
+        process.env.LOG_LEVEL = "debug";
+    }else{
+        process.env.LOG_LEVEL = "info";
+    }
+    log.level = process.env.LOG_LEVEL;
+}
+
+// 检查环境变量
+function checkEnv(){
+    const envPath = path.resolve(userHome,'.env');
+    if(pathExists(envPath)){
+        env.config({
+            path:envPath
+        })
+    }
+    createDefaultConfig()
+}
+// 创建默认配置项
+function createDefaultConfig(){
+    const cliConfig = {
+        home: userHome
+    }
+    if (process.env.CLI_HOME) {
+        cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
+    } else {
+        cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+    }
+    process.env.CLI_HOME_PATH = cliConfig.cliHome;
 }

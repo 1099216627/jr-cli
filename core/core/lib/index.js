f276510e77b4d0d809839037c893a38f0c1b3e53
fengliushijie <1099216627@qq.com>
2023-06-14 18:54:13 +0800

    🚀root权限自动降级

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -6,12 +6,14 @@ const log = require("@jr-cli/log");
 const semver = require("semver");
 const colors = require("colors")
 const pkg = require("../package.json");
+const root = require("root-check")
 const constant = require("./const");
 
 function core(args) {
     try{
         checkPkgVersion();
         checkNodeVersion();
+        rootDegradation();
     }catch (e) {
         log.error(e.message)
     }
@@ -29,5 +31,10 @@ function checkNodeVersion () {
     if(semver.lt(curNodeVersion,lowestNodeVersion)){
         throw new Error(colors.red(`Node版本过低，请升级 ${lowestNodeVersion} 以上的版本`))
     }
+}
+// root权限降级
+function rootDegradation(){
+    // 当root权限开启时自动降级
+    root();
 }
 

1fe26c38b5b6675af406fdb14745fe832bd511f1
fengliushijie <1099216627@qq.com>
2023-06-14 21:40:51 +0800

    🚀通用npm功能封装 版本更新检查提示完成

---
+++ utils/npm/__tests__/npm.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const npm = require('../lib');
+
+describe('npm', () => {
+    it('needs tests');
+});
---
+++ utils/npm/lib/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const urlJoin = require("url-join");
+const semver = require("semver");
+const axiosInstance = require("@jr-cli/http").getInstance();
+
+async function getPkgInfoByNpm(pkgName,curVersion, registry) {
+    if (!pkgName) {
+        return null;
+    }
+    registry = registry || getDefaultRegistry();
+    const finallyUrl = urlJoin(registry, pkgName);
+    const result = await axiosInstance.get(finallyUrl);
+    const versions = getPackageVersions(result);
+    const newVersions = getOutdatedPkgVersions(versions,curVersion);
+    if(newVersions && newVersions.length > 0){
+        return newVersions[0]
+    }
+    return null;
+}
+
+// 获取线上源地址，默认官方
+function getDefaultRegistry(isOrigin = true) {
+    return isOrigin ? "https://registry.npmjs.org" : "https://registry.npmmirror.com/";
+}
+
+// 获取指定包所有版本号
+function getPackageVersions(data) {
+    if (data) {
+        return Object.keys(data.versions)
+    } else {
+        return []
+    }
+}
+// 获取高版本包版本号
+function getOutdatedPkgVersions (versions,curVersion) {
+    return versions.filter(v => semver.satisfies(v,`^${curVersion}`)).sort((a,b) => semver.gt(b,a))
+}
+
+module.exports = {
+    getPkgInfoByNpm
+};
